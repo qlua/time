0f46b763129857035f49347baa681e4ae500887b
qlua <michael.bulychev@gmail.com>
2015-09-24 16:23:08 +0600

    1

--- test.lua
+++ test.lua
@@ -4,15 +4,11 @@ function fmessage(fmt, ...)
 	message(fmt:format(...))
 end
 
+
 function main()
-	time = time.get_system_time()
-	for k, v in pairs(time) do
+	sys_time = time.get_system_time()
+	for k, v in pairs(sys_time) do
 		fmessage("%s : %d", k, v)
 	end
 end
-function _main()
-	message = print
-	main()
-end
 
-_main()
